3fe3a75ad40b7db17c36f5902e6742a5f0a3adb1
Leo1998 <leonard.fricke98@gmail.com>
2025-10-29 15:38:41 +0100

    update to opengl 3.3

--- src/pmp/visualization/window.cpp
+++ src/pmp/visualization/window.cpp
@@ -53,7 +53,7 @@ Window::Window(const char* title, int width, int height, bool showgui)
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_SAMPLES, 4);
     glfwWindowHint(GLFW_SCALE_TO_MONITOR, GLFW_TRUE);
 
